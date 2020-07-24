a166bca2e746402a86c7466f90077e4f24335958
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2020-07-23 21:31:20 -0400

    Corrected logic and added LUT

--- js/index.js
+++ js/index.js
@@ -14,20 +14,23 @@ $(document).ready(function() {
 	if (h > 11) {
 		if (h > 12) {
 			h = h - 12;
+			clock = 'THE TIME IS NOW:' + '<br><span>' + h + ':' + m + 'PM</span>';
+			h = h + 12;
+		} else {
+			clock = 'THE TIME IS NOW:' + '<br><span>' + h + ':' + m + 'PM</span>';
 		}
-		clock = 'THE TIME IS NOW:' + '<br><span>' + h + ':' + m + 'PM</span>';
 	} else {
 		clock = 'THE TIME IS NOW:' + '<br><span>' + h + ':' + m + 'AM</span>';
 	}
 
 	//Grace sleeps from 9PM to 7AM
 	var isAsleep;
-	if (6 < h < 21) {
+	if (6 < h && h < 21) {
 		isAsleep = 'NO, GRACE IS:<br><span>NOT ASLEEP</span>';
 		rand = Math.floor(Math.random() * 5) + 1; //random number from 1 to 5
 		imageUrl = './img/awake' + rand + '.jpg';
 	} else {
-		isAsleep = 'YES, GRACE IS:<br>ASLEEP</span>';
+		isAsleep = 'YES, GRACE IS:<br><span>ASLEEP</span>';
 		rand = Math.floor(Math.random() * 3) + 1; //random number from 1 to 3
 		imageUrl = './img/asleep' + rand + '.jpg';
 	}
